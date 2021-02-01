fdbcba4449c5e37df50d5998db28977c56f6cb75
DNA5769 <dennisthomas2002@gmail.com>
2021-02-01 21:39:28 +0530

    Implemented Knight moves

--- pieces/Knight.py
+++ pieces/Knight.py
@@ -3,3 +3,16 @@ class Knight:
     self.color = color
     self.pos = (r, c)
     self.img = f'img/{color}H.png'
+
+  def getPossibleMoves(self, board):
+    moves = []
+    r, c = self.pos
+
+    for x, y in [(-2, -1), (-1, -2), (2, -1), (1, -2), (-2, 1), (-1, 2), (2, 1), (1, 2)]:
+      posr = r+y
+      posc = c+x
+      
+      if 0 <= posr <= 7 and 0 <= posc <= 7 and (board[posr][posc] is None or board[posr][posc].color != self.color):
+        moves.append((posr, posc))
+
+    return moves
