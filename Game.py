fd9349504e495f5829f219bf2b8e93c0dffc4463
DNA5769 <dennisthomas2002@gmail.com>
2021-02-01 15:52:04 +0530

    Modified board to add Pawns

--- Game.py
+++ Game.py
@@ -1,13 +1,17 @@
+from Pawn import Pawn
+
 class Game:
   def __init__(self):
     self.board = getBoard()
 
   def getBoard(self):
-    board = []
-
-    for i in range(8):
-      board.append([])
-      for j in range(8):
-        board[i].append([(i,j)])
-
-    return board
+    return [
+      [None]*8,
+      [Pawn(1, c, 'B') for c in range(8)],
+      [None]*8,
+      [None]*8,
+      [None]*8,
+      [None]*8,
+      [Pawn(6, c, 'W') for c in range(8)],
+      [None]*8,
+    ]
