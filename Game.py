59ad9251bbc551b1a62307625f82309711da48c5
DNA5769 <dennisthomas2002@gmail.com>
2021-02-01 19:48:51 +0530

    Added Bishops to the board

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 from pieces.Pawn import Pawn
 from pieces.King import King
 from pieces.Queen import Queen
+from pieces.Bishop import Bishop
 
 class Game:
   def __init__(self):
@@ -8,12 +9,12 @@ class Game:
 
   def getBoard(self):
     return [
-      [None, None, None, Queen('B'), King('B'), None, None, None],
+      [None, None, Bishop(0, 2, 'B'), Queen('B'), King('B'), Bishop(0, 5, 'B'), None, None],
       [Pawn(1, c, 'B') for c in range(8)],
       [None]*8,
       [None]*8,
       [None]*8,
       [None]*8,
       [Pawn(6, c, 'W') for c in range(8)],
-      [None, None, None, Queen('W'), King('W'), None, None, None],
+      [None, None, Bishop(7, 2, 'W'), Queen('W'), King('W'), Bishop(7, 5, 'W'), None, None],
     ]
