4638674a51c51451abcd5f52619b6a6aecd2fa0f
DNA5769 <dennisthomas2002@gmail.com>
2021-02-01 15:32:36 +0530

    Added board to Game.py

--- Game.py
+++ Game.py
@@ -1,3 +1,13 @@
 class Game:
   def __init__(self):
-    self.board = [[None]*8 for _ in range(8)]
+    self.board = getBoard()
+
+  def getBoard(self):
+    board = []
+
+    for i in range(8):
+      board.append([])
+      for j in range(8):
+        board[i].append([(i,j)])
+
+    return board
